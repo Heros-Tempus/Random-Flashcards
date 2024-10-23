717816f656bd8af38974e24cba954c89b8bb861d
Tim Miller <tim_j_miller@yahoo.com>
2024-10-23 18:46:15 -0400

    Added code to read in the csv input

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using static System.Linq.Enumerable;
 
 namespace Random_Flashcards
 {
@@ -16,9 +19,61 @@ namespace Random_Flashcards
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Random rnd = new Random();
+        string CardListLocation = Microsoft.VisualBasic.FileSystem.CurDir() + "\\Card List.csv";
+        static List<List<String>> CardSets = new List<List<String>>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            if (File.Exists(CardListLocation))
+            {
+                using (TextFieldParser parser = new TextFieldParser(CardListLocation))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    string[]? header = parser.ReadFields();
+                    if (header != null && header.Length > 0)
+                    {
+                        foreach(string category in header)
+                        {
+                            CardSets.Add(new List<String> { category });
+
+                            //todo: create tab control for the category
+                        }
+                        while (!parser.EndOfData)
+                        {
+                            string[]? fields = parser.ReadFields();
+
+                            if (fields != null && fields.Length > 0)
+                            {
+                                foreach (var i in Range(0, header.Length))
+                                {
+                                    if (fields[i] != "")
+                                    {
+                                        CardSets[i].Add(fields[i]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //yell at user for making an empty csv
+                    }
+                }
+            }
+            else
+            {
+                //yell at user for deleting the file
+            }
+        }
+
+
     }
 }
