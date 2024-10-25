cc19a7a76724d70edb954ee02d69d73c9e48dc7f
Tim Miller <tim_j_miller@yahoo.com>
2024-10-24 20:15:04 -0400

    Added DispatcherTimer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,9 +8,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using static System.Linq.Enumerable;
+using System.Windows.Threading;
 
 namespace Random_Flashcards
 {
@@ -22,10 +24,28 @@ namespace Random_Flashcards
         static Random rnd = new Random();
         string CardListLocation = Microsoft.VisualBasic.FileSystem.CurDir() + "\\Card List.csv";
         static List<List<String>> CardSets = new List<List<String>>();
+        static DispatcherTimer dt = new DispatcherTimer();
+        static int dt_counter = 90;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            dt.Tick += new EventHandler(DT_Tick);
+            dt.Interval = TimeSpan.FromMilliseconds(33); //one tick per frame at a rate of 30fps
+        }
+
+        private void DT_Tick(object? sender, EventArgs e)
+        {
+            if (dt_counter % 3 == 0) 
+            {
+                //pick random element
+            }
+            if (dt_counter <= 0)
+            {
+                dt.Stop();
+            }
+            dt_counter--;
         }
 
         private void Window_Initialized(object sender, EventArgs e)
@@ -45,6 +65,20 @@ namespace Random_Flashcards
                             CardSets.Add(new List<String> { category });
 
                             //todo: create tab control for the category
+                            TextBlock textBlock = new TextBlock();
+                            textBlock.Text = "fff" + category;
+                            textBlock.Name = "item";
+                            textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                            textBlock.VerticalAlignment = VerticalAlignment.Center;
+                            textBlock.Foreground = new SolidColorBrush(new Color() { A = 255, R = 100, G = 50, B = 0});
+                            textBlock.FontSize = 50;
+
+                            TabItem item = new TabItem();
+                            item.Header = category;
+                            item.Content = textBlock;
+
+                            Tab_Control.Items.Add(item);
+                            
                         }
                         while (!parser.EndOfData)
                         {
@@ -72,8 +106,14 @@ namespace Random_Flashcards
             {
                 //yell at user for deleting the file
             }
+
         }
 
+        private void TabControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            dt_counter = 100;
+            dt.Start();
+        }
 
     }
 }
